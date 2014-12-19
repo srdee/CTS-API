d8eadc85191b79af0dc5872574739407866ad364
Thibault Clérice <leponteineptique@gmail.com>
2014-12-19 11:00:34 +0100

    Fixing entities errors, adding a new error when file does not exist

--- cts/xml/helpers.py
+++ cts/xml/helpers.py
@@ -2,6 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import xml.etree.ElementTree as ElementTree
+from codecs import *
+import re
+import os.path
+
+entities = re.compile("(\&[a-zA-Z0-9\.]+\;)")
+
+
+def removeEntities(path):
+    """ Open a file, remove its entities and send back its content
+
+    :param path: Path of the file to open
+    :type path: str or unicode
+    :returns: Content of the XML File (- Entities)
+    :rtype: str or unicode
+    """
+    if os.path.isfile(path) is False:
+        raise IOError("File {0} does not exist".format(path))
+
+    with open(path, "r") as f:
+        xml = f.read()
+        xml = entities.sub("", xml)
+        return xml
+
+    return None
 
 
 def xmlParsing(xml=None):
@@ -14,7 +38,7 @@ def xmlParsing(xml=None):
         if "<" in xml and ">" in xml:
             return ElementTree.fromstring(xml)
         else:
-            return ElementTree.parse(xml).getroot()
+            return ElementTree.fromstring(removeEntities(xml))
     elif isinstance(xml, ElementTree.Element):
         return xml
     else:
